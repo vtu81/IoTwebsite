2d192bffa43972a8c1fd12821ccf8d6004caa273
vtu81 <48283858+vtu81@users.noreply.github.com>
2021-06-28 22:14:09 +0800

    Update account with a taken email bug fixed

--- server/routes/update_account.js
+++ server/routes/update_account.js
@@ -1,4 +1,4 @@
-const { RateReview } = require('@material-ui/icons');
+const { RateReview, KeyboardReturnOutlined } = require('@material-ui/icons');
 var express = require('express');
 var router = express.Router();
 var sql = require('../utils/sql.js');
@@ -8,8 +8,8 @@ var query = sql.query;
 router.post('/', function(req, res, next) {
   console.log('Backend received post body: ', req.body);
 
-  var sql_command = 'update account set email = ?, phone_number = ? where user_name = ?';
-  query(sql_command, [req.body.email, req.body.phone_number, req.body.user_name], function (err, result) {
+  var sql_command = 'select count(user_name) as num from account where email = ? and user_name != ?';
+  query(sql_command, [req.body.email, req.body.user_name], function (err, result) {
     if(err){
       console.log('[SELECT ERROR] - ', err.message);
       res.send({
@@ -19,11 +19,36 @@ router.post('/', function(req, res, next) {
       throw err;
       return;
     }
-    res.send({
-      'success': true,
-      'msg': 'Update account info successfully!'
-    });
+    if(result[0].num > 0)
+    {
+      res.send({
+        'success': false,
+        'msg': 'Update account failed! The email has already been registered by another user.'
+      });
+      return;
+    }
+    else
+    {
+      sql_command = 'update account set email = ?, phone_number = ? where user_name = ?';
+      query(sql_command, [req.body.email, req.body.phone_number, req.body.user_name], function (err, result) {
+        if(err){
+          console.log('[SELECT ERROR] - ', err.message);
+          res.send({
+            'success': false,
+            'msg': 'Update account info failed due to internal server error, please try again later.'
+          });
+          throw err;
+          return;
+        }
+        res.send({
+          'success': true,
+          'msg': 'Update account info successfully!'
+        });
+      });
+    }
   });
+
+  
 });
 
 module.exports = router;
